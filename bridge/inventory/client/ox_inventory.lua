8c1a6b59e9fd3d2f3a0000ce1fc15e13149a9fa1
Greve <141843261+PlazaRollespillDEV@users.noreply.github.com>
2023-11-13 15:40:54 +0100

    Stash sounds - when opening (Ox_Inventory)

--- bridge/inventory/client/ox_inventory.lua
+++ bridge/inventory/client/ox_inventory.lua
@@ -2,4 +2,5 @@ if GetResourceState('ox_inventory') ~= 'started' then return end
 
 OpenStash = function(data,identifier)
     TriggerEvent('ox_inventory:openInventory', 'stash', {id = 'stash_'..data.motel..'_'..identifier..'_'..data.index, name = 'Storage', slots = 70, weight = 70000, coords = GetEntityCoords(cache.ped)})
+    TriggerServerEvent('InteractSound_SV:PlayOnSource', 0.5, 'StashOpen', 1.0)
 end
